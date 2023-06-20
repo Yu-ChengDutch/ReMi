640facdf27f6e6fc85f943489635f37cbd0cdf45
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-06-20 13:44:07 +0200

    Check if function works like this

--- functions.js
+++ functions.js
@@ -1,24 +1,26 @@
 // Setting all the lines
 
-let top_line = document.getElementById('top-line');
-let line_1 = document.getElementById('line-1');
-let line_2 = document.getElementById('line-2');
-let line_3 = document.getElementById('line-3');
-let line_4 = document.getElementById('line-4');
-
-let lines = [top_line, line_1, line_2, line_3, line_4];
-
-top_line.innerText = "Check";
-
-document.getElementById('line-1').innerText = "Check";
-
-// Check by commenting
-
-console.log("Welcome!");
-
-// Empty all lines
-
-for (i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
-    lines[i].innerHTML = "";
+function setUp() {
+    let top_line = document.getElementById('top-line');
+    let line_1 = document.getElementById('line-1');
+    let line_2 = document.getElementById('line-2');
+    let line_3 = document.getElementById('line-3');
+    let line_4 = document.getElementById('line-4');
+    
+    let lines = [top_line, line_1, line_2, line_3, line_4];
+    
+    top_line.innerText = "Check";
+    
+    document.getElementById('line-1').innerText = "Check";
+    
+    // Check by commenting
+    
+    console.log("Welcome!");
+    
+    // Empty all lines
+    
+    for (i = 0; i < lines.length; i++) {
+        console.log(lines[i]);
+        lines[i].innerHTML = "";
+    }
 }
